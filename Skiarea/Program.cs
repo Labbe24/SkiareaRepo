95ad38fa83e4d7d56062196276394ccade420350
Lasse Andresen <lasseandresen.1996@hotmail.com>
2020-10-21 13:40:36 +0200

    Add project files.

---
+++ Skiarea/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Skiarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
